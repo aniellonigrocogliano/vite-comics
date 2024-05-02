6ec8bfccd28e35b140ef078d3f07e9132ba78b50
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-05-02 18:52:34 +0200

    add: logo e menu

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,106 @@
+<script>
+export default {
+  data() {
+    return {
+      menu: [
+        {
+          title: "CHARACTERS",
+          isActive: false,
+        },
+        {
+          title: "COMICS",
+          isActive: true,
+        },
+        {
+          title: "MOVIES",
+          isActive: false,
+        }, 
+        {
+          title: "TV",
+          isActive: false,
+        },
+        {
+          title: "GAMES",
+          isActive: false,
+        },
+        {
+          title: "COLLECTIBLES",
+          isActive: false,
+        },
+        {
+          title: "VIDEOS",
+          isActive: false,
+        },
+        {
+          title: "FANS",
+          isActive: false,
+        },
+        {
+          title: "NEWS",
+          isActive: false,
+        },
+        {
+          title: "SHOP",
+          isActive: false,
+        },
+     
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <section class="section-header">
+    <div class="logo">
+      <img
+        src="../assets/img/dc-logo.png"
+        alt="Logo DC Comics"
+      />
+    </div>
+    <ul class="navigation">
+      <li v-for="item in menu">
+        {{item.title}}
+      </li>
+    </ul>
+</section>
+  </header>
+</template>
+
+<style scoped lang="scss">
+@use "../style/partials/mixin" as *;
+header { @include flex(row, center, center);
+    
+    .section-header{width:80%;
+        @include flex(row, space-between, center);
+        padding: 15px;
+}
+    
+  .logo {
+    text-align: center;
+  }
+  ul {
+    @include flex(row, center, center);
+    list-style-type: none;
+    gap: 1rem;
+    margin-top: 26px;
+
+    a {
+      padding: 10px 16px;
+      text-decoration: none;
+      display: inline-block;
+
+      &:hover {
+        
+      }
+
+      &.active {
+        
+      }
+    }
+  }
+
+}
+
+</style>
